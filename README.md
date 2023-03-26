a67c74e5ee6199d08bc60b0289ec1a9a585e1000
spark8899 <sparkzhang8899@gmail.com>
2023-03-26 21:22:19 +0800

    add monitor eth balance

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ go eth tools
 https://github.com/miguelmota/ethereum-development-with-go-book/tree/master/code
 
 
+# monitor eth balance
+build
+```
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o monitor_eth_balance_linux64 monitor_eth_balance.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o monitor_eth_balance_windows64 monitor_eth_balance.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o monitor_eth_balance_darwin64 monitor_eth_balance.go
+```
---
+++ monitor_eth_balance.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"math"
+	"math/big"
+	"net/http"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	EthRpc       string
+	BalanceAlert float64
+	QywxBot      []string
+	EthAddress   []string
+}
+
+type WechatMessage struct {
+	MsgType string            `json:"msgtype"`
+	Text    WechatTextMessage `json:"markdown"`
+}
+
+type WechatTextMessage struct {
+	Content string `json:"content"`
+}
+
+func main() {
+	// load Config
+	configPath := "eth_balance.yaml"
+	config, err := LoadConfig(configPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var msg string = "### ETH Balance Alert:\n"
+	for _, addr := range config.EthAddress {
+		balance, err := GetEthBalance(addr, config.EthRpc)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf("%s: %.4f\n", addr, balance)
+
+		if balance < config.BalanceAlert {
+			alertInfo := fmt.Sprintf("%s *%.4f*\n", addr, balance)
+			msg = fmt.Sprintf("%s%s", msg, alertInfo)
+		}
+	}
+
+	fmt.Println(len(msg))
+	if len(msg) > 30 {
+		for _, chatKey := range config.QywxBot {
+			SendQYWX(chatKey, msg)
+		}
+	}
+}
+
+func LoadConfig(configPath string) (*Config, error) {
+	viper.SetConfigFile(configPath)
+	err := viper.ReadInConfig()
+	if err != nil {
+		return nil, fmt.Errorf("Error loading config file: %s", err.Error())
+	}
+
+	var config Config
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		return nil, fmt.Errorf("Error unmarshaling config file: %s", err.Error())
+	}
+
+	return &config, nil
+}
+
+func GetEthBalance(addressStr string, ethRpc string) (float64, error) {
+	client, err := ethclient.Dial(ethRpc)
+	if err != nil {
+		return 0, fmt.Errorf("Error connect Ethrpc: %s", err.Error())
+	}
+
+	address := common.HexToAddress(addressStr)
+	balance, err := client.BalanceAt(context.Background(), address, nil)
+	if err != nil {
+		return 0, fmt.Errorf("Error %s get balance: %s", addressStr, err.Error())
+	}
+	return ConvertEther(balance), nil
+}
+
+// convert to ether
+func ConvertEther(balance *big.Int) float64 {
+	value := new(big.Float).SetInt(balance)
+	etherValue := new(big.Float).Quo(value, big.NewFloat(math.Pow10(18)))
+	result, _ := etherValue.Float64()
+	return result
+}
+
+func SendQYWX(chatKey string, messageContent string) {
+	webhookURL := fmt.Sprintf("https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=%s", chatKey)
+
+	// create WechatMessage
+	message := &WechatMessage{
+		MsgType: "markdown",
+		Text: WechatTextMessage{
+			Content: messageContent,
+		},
+	}
+
+	// send qywx
+	requestBody, _ := json.Marshal(message)
+	response, err := http.Post(webhookURL, "application/json", bytes.NewBuffer(requestBody))
+	if err != nil {
+		fmt.Println("Failed to send wechat message: ", err)
+	} else {
+		fmt.Println("Wechat message sent successfully with status code: ", response.StatusCode)
+	}
+}
